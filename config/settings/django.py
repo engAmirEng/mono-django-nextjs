ca78e9684fd668d283aa681e491cad428269e62b
AmirW <eng.amir.bu@gmail.com>
2023-07-18 16:26:12 +0330

    fix detection of whitenoise.runserver_nostatic

--- config/settings/django.py
+++ config/settings/django.py
@@ -121,7 +121,7 @@ LOCAL_APPS = [
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
-INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+INSTALLED_APPS = LOCAL_APPS + THIRD_PARTY_APPS + DJANGO_APPS
 
 # MIGRATIONS
 # ------------------------------------------------------------------------------
