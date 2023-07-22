3eba07534946e7dd2617119eda2ad2a78397b2ec
AmirW <eng.amir.bu@gmail.com>
2023-07-22 20:12:25 +0330

    celery minor changes

--- name_goes_here/users/tasks.py
+++ name_goes_here/users/tasks.py
@@ -1,18 +1,18 @@
 from django.contrib.auth import get_user_model
 from django.core import management
 
-from config import celery_app
+from celery import shared_task
 
 User = get_user_model()
 
 
-@celery_app.task()
+@shared_task
 def get_users_count():
     """A pointless Celery task to demonstrate usage."""
     return User.objects.count()
 
 
-@celery_app.task()
+@shared_task
 def clear_expired_refresh_tokens():
     """Removes expired refresh tokens from db."""
     return management.call_command("cleartokens", expired=True)
