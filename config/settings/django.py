86f8d04db32a965a848303d95621641fbccdc701
Amir Khalife <eng.amir.bu@gmail.com>
2024-01-02 22:59:33 +0330

    rm django_extensions

--- config/settings/django.py
+++ config/settings/django.py
@@ -120,7 +120,6 @@ THIRD_PARTY_APPS = clean_ellipsis(
         "corsheaders",
         "debug_toolbar" if PLUGGABLE_FUNCS.DEBUG_TOOLBAR else ...,
         "django_celery_beat",
-        "django_extensions" if PLUGGABLE_FUNCS.EXTENSIONS else ...,
         "django_filters",
         "drf_spectacular",
         "graphene_django",
